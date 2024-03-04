5f508d7c277581026da756b46a51ab20c14404d8
taduyhieu2004 <hieu391968@gmail.com>
2024-03-04 21:25:48 +0700

    duyhieu.

--- src/main/java/com/example/quanlichitieu/exception/base/ConflictException.java
+++ src/main/java/com/example/quanlichitieu/exception/base/ConflictException.java
@@ -4,15 +4,15 @@ package com.example.quanlichitieu.exception.base;
 import static com.example.quanlichitieu.constant.Constant.StatusException.CONFLICT;
 
 public class ConflictException extends BaseException {
-  public ConflictException(String id, String objectName) {
-    setStatus(CONFLICT);
-    setCode("com.lawman.shop_sport.exception.base.ConflictException");
-    addParam("id", id);
-    addParam("objectName", objectName);
-  }
+    public ConflictException(String id, String objectName) {
+        setStatus(CONFLICT);
+        setCode("com.lawman.shop_sport.exception.base.ConflictException");
+        addParam("id", id);
+        addParam("objectName", objectName);
+    }
 
-  public ConflictException(){
-    setStatus(CONFLICT);
-    setCode("com.lawman.shop_sport.exception.base.ConflictException");
-  }
+    public ConflictException() {
+        setStatus(CONFLICT);
+        setCode("com.lawman.shop_sport.exception.base.ConflictException");
+    }
 }
